7ee5ac08d40141fb49e7ef54a1eb86e89d6897a0
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-11-30 19:36:21 +0200

    Safely Storing Sensitive Configuration Data

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -26,7 +26,8 @@ namespace CityInfo.API
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appSettings.json", optional:false, reloadOnChange:true)
-                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional:true, reloadOnChange:true);
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional:true, reloadOnChange:true)
+                .AddEnvironmentVariables();
 
             Configuration = builder.Build();
         }
@@ -51,7 +52,7 @@ namespace CityInfo.API
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
-            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CityInfoDB;Trusted_Connection=True;";
+            var connectionString = Startup.Configuration["connectionStrings:cityInfoDBConnectionString"];
             services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
         }
 
